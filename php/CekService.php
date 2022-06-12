5f635c38c6143fe171a88bd92fee07de2345316e
Yovi Adhi Ramadani <96305225+Ramadani354@users.noreply.github.com>
2022-06-12 16:35:19 +0700

    Add files via upload

---
+++ php/CekService.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Serpisan</title>
+  </head>
+  <body>
+
+      <div class="side">
+        <h2>Cek Service</h2>
+        <hr>
+        <p>Mohon ditunggu dan cek secara berkala.</p>
+      </div>
+
+      <div class="gambarpenerimaan">
+          <img src="penerimaan.png">
+      </div>
+
+      <div class="textpenerimaan">
+        Penerimaan
+      </div>
+
+      <div class="gambarrepair">
+        <img src="repair.png">
+      </div>
+
+      <div class="textrepair">
+        <b>Proses Perbaikan</b>
+      </div>
+
+      <div class="keterangantextrepair">
+        Sedang dalam proses perbaikan, mohon sabar ditunggu
+      </div>
+
+      <div class="gambarselesai">
+        <img src="selesai.png">
+      </div>
+
+      <div class="textselesai">
+        <b>Selesai</b>
+      </div>
+
+      <div class="gambarpembayaran">
+        <img src="pembayaran.png">
+      </div>
+
+      <div class="textpembayaran">
+        <b>Pembayaran</b>
+      </div>
+
+      <div class="line1">
+      </div>
+
+      <div class="line2">
+      </div>
+
+      <div class="gambarhp">
+        <img src="hp.png">
+      </div>
+
+
+      <!-- Optional JavaScript; choose one of the two! -->
+
+      <!-- Option 1: Bootstrap Bundle with Popper -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+      <!-- Option 2: Separate Popper and Bootstrap JS -->
+      <!--
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+      -->
+    
+  </body>
+</html>
