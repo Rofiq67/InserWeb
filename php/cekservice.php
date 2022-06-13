36136690bd59c34e5d8d40bdc04a62c2d34249c1
Yovi Adhi Ramadani <96305225+Ramadani354@users.noreply.github.com>
2022-06-13 17:29:21 +0700

    Update cekservice.php

--- php/cekservice.php
+++ php/cekservice.php
@@ -1,17 +1,23 @@
+<?php 
+   include 'navcustomer.php';
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css.css">
+    <link rel="stylesheet" href="CekService.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Google Font -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <title>Serpisan</title>
   </head>
-  <body>
 
+  <body>
       <div class="side">
         <h2>Cek Service</h2>
         <hr>
@@ -19,7 +25,7 @@
       </div>
 
       <div class="gambarpenerimaan">
-          <img src="images/penerimaan.png">
+        <img src="images/penerimaan.png">
       </div>
 
       <div class="textpenerimaan">
@@ -63,6 +69,13 @@
       <div class="gambarhp">
         <img src="images/hp1.png">
       </div>
+        
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+      <?php 
+          include 'bawah.php';
+      ?>
 
 
       <!-- Optional JavaScript; choose one of the two! -->
