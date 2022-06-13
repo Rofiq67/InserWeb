767741056b5cbd0eaeae5c9369d77894c62d916c
Fiq <75403740+Rofiq67@users.noreply.github.com>
2022-06-13 17:59:00 +0700

    Update cekservice.php

--- php/cekservice.php
+++ php/cekservice.php
@@ -1,93 +1,93 @@
-<?php 
-   include 'navcustomer.php';
-?>
+    <?php 
+    include 'navcustomer.php';
+    ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="CekService.css">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- Google Font -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/cekcs.css">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- Google Font -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    <title>Serpisan</title>
-  </head>
+        <title>Serpisan</title>
+    </head>
 
-  <body>
-      <div class="side">
-        <h2>Cek Service</h2>
-        <hr>
-        <p>Mohon ditunggu dan cek secara berkala.</p>
-      </div>
+    <body>
+        <div class="side">
+            <h2>Cek Service</h2>
+            <hr>
+            <p>Mohon ditunggu dan cek secara berkala.</p>
+        </div>
 
-      <div class="gambarpenerimaan">
-        <img src="images/penerimaan.png">
-      </div>
+        <div class="gambarpenerimaan">
+            <img src="images/penerimaan.png">
+        </div>
 
-      <div class="textpenerimaan">
-        Penerimaan
-      </div>
+        <div class="textpenerimaan">
+            Penerimaan
+        </div>
 
-      <div class="gambarrepair">
-        <img src="images/repair.png">
-      </div>
+        <div class="gambarrepair">
+            <img src="images/repair.png">
+        </div>
 
-      <div class="textrepair">
-        <b>Proses Perbaikan</b>
-      </div>
+        <div class="textrepair">
+            <b>Proses Perbaikan</b>
+        </div>
 
-      <div class="keterangantextrepair">
-        Sedang dalam proses perbaikan, mohon sabar ditunggu
-      </div>
+        <div class="keterangantextrepair">
+            Sedang dalam proses perbaikan, mohon sabar ditunggu
+        </div>
 
-      <div class="gambarselesai">
-        <img src="images/selesai.png">
-      </div>
+        <div class="gambarselesai">
+            <img src="images/selesai.png">
+        </div>
 
-      <div class="textselesai">
-        <b>Selesai</b>
-      </div>
+        <div class="textselesai">
+            <b>Selesai</b>
+        </div>
 
-      <div class="gambarpembayaran">
-        <img src="images/pembayaran.png">
-      </div>
+        <div class="gambarpembayaran">
+            <img src="images/pembayaran.png">
+        </div>
 
-      <div class="textpembayaran">
-        <b>Pembayaran</b>
-      </div>
+        <div class="textpembayaran">
+            <b>Pembayaran</b>
+        </div>
 
-      <div class="line1">
-      </div>
+        <div class="line1">
+        </div>
 
-      <div class="line2">
-      </div>
+        <div class="line2">
+        </div>
 
-      <div class="gambarhp">
-        <img src="images/hp1.png">
-      </div>
+        <div class="gambarhp">
+            <img src="images/hpajor.jpg">
+        </div>
+            
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br>
+
+        <?php 
+            include 'bawah.php';
+        ?>
+
+
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+        -->
         
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-
-      <?php 
-          include 'bawah.php';
-      ?>
-
-
-      <!-- Optional JavaScript; choose one of the two! -->
-
-      <!-- Option 1: Bootstrap Bundle with Popper -->
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-      <!-- Option 2: Separate Popper and Bootstrap JS -->
-      <!--
-      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-      -->
-    
-  </body>
-</html>
+    </body>
+    </html>
