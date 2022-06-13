a7b5a7c35f8bfb2b2cc0b324cf8933674dcab691
Fiq <75403740+Rofiq67@users.noreply.github.com>
2022-06-13 18:00:30 +0700

    Update navadmin.php

--- php/navadmin.php
+++ php/navadmin.php
@@ -5,21 +5,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title> Serpisan | Admin </title>
+
+        <script type="text/javascript" src="Scripts/jquery-2.1.1.min.js"></script>
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <!-- Google Font -->
         <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
         <!-- Box Icon -->
         <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-
-
-        <title> Serpisa | Admin </title>
-
-        <style>
-
         
-
-        </style>
     </head>
     <body style="font-family: Nunito; background-color: #f9f9f9;">
 
@@ -37,20 +32,22 @@
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item" style="margin-right: 32px;">
-                <a class="nav-link" href="#">Cek Service</a>
+                <a class="nav-link" href="tabeladmin.php">Cek Service</a>
                 </li>
                 <li class="nav-item" style="margin-right: 32px;">
-                <a class="nav-link" href="#">Layanan</a>
-                </li>
-                <li class="nav-item" style="margin-right: 32px;">
-                <a class="nav-link" href="#">About me</a>
+                <a class="nav-link" href="#aboutme">About me</a>
                 </li>
                 </li>
                 <li class="nav-item" style="margin-right: -4px;">
                     <a class="nav-link" href="#"><i class='bx bxs-user-circle' style="font-size: 15pt;"></i></a>
                 </li>
-                <li class="nav-item" style="margin-right:0px;">
-                <a class="nav-link" href="#">Admin</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Admin
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Sign Out</a></li>
+                    </ul>
                 </li>
             </ul>
             </div>
@@ -59,7 +56,6 @@
     
 
     
-
 
 
 
