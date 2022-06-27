14e5346351c93540a0fad63e07ce53f292fdf7c8
Fiq <75403740+Rofiq67@users.noreply.github.com>
2022-06-27 11:35:54 +0700

    Update navadmin.php

--- php/navadmin.php
+++ php/navadmin.php
@@ -5,16 +5,22 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title> Serpisan | Admin </title>
-
-        <script type="text/javascript" src="Scripts/jquery-2.1.1.min.js"></script>
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <!-- Google Font -->
         <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
         <!-- Box Icon -->
         <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+        <link href="css/navcs.css" type="text/css"  rel='stylesheet'>
+
+
+        <title> Serpisan | Customer </title>
+
+        <style>
+
         
+
+        </style>
     </head>
     <body style="font-family: Nunito; background-color: #f9f9f9;">
 
@@ -28,11 +34,14 @@
         </a>
             <div class="collapse navbar-collapse" id="navbarNavDropdown" style="margin-left: 554px; font-size:16px; font-weight: 700;">
             <ul class="navbar-nav">
-                <li class="nav-item" style="margin-right: 32px;">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <li class="nav-item active" style="margin-right: 32px;">
+                <a class="nav-link " aria-current="page" href="index.php">Home</a>
                 </li>
                 <li class="nav-item" style="margin-right: 32px;">
-                <a class="nav-link" href="tabeladmin.php">Cek Service</a>
+                <a class="nav-link" href="cekservice.php">Cek Service</a>
+                </li>
+                <li class="nav-item" style="margin-right: 32px;">
+                <a class="nav-link" href="layanan.php">Layanan</a>
                 </li>
                 <li class="nav-item" style="margin-right: 32px;">
                 <a class="nav-link" href="#aboutme">About me</a>
@@ -41,12 +50,13 @@
                 <li class="nav-item" style="margin-right: -4px;">
                     <a class="nav-link" href="#"><i class='bx bxs-user-circle' style="font-size: 15pt;"></i></a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Admin
+                <li class="nav-item dropdown" style="font-size:16px; font-weight: 700;">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Customer
                     </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Sign Out</a></li>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a href="#" class="dropdown-item"> Profil </a></li>
+                        <li><a class="dropdown-item" href="logout.php"><i class='bx bx-log-out'></i> Sign Out </a></li>
                     </ul>
                 </li>
             </ul>
@@ -57,14 +67,25 @@
 
     
 
+    <script>
+        $(".nav .navbar-nav a").on("click", function(){
+        $(".nav .navbar-nav").find("li.active").removeClass("active");
+        $(this).parent('li').addClass("active");
+        });
+    </script>
 
 
 
 
 
-
+    
     <!-- Optional JavaScript; choose one of the two! -->
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+    <!--jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 
+    <script src="js/garisnav.js"></script>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
