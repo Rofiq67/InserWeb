b5eddebc930c5d1d63a33aaaa0d7ce4f76d2ae8d
Fiq <75403740+Rofiq67@users.noreply.github.com>
2022-06-27 12:21:16 +0700

    Add files via upload

---
+++ php/about.php
@@ -0,0 +1,112 @@
+<?php 
+    include 'navcustomer.php';
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- Google Font -->
+        <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+        <!-- Box Icon -->
+        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+        <!-- Bootstrap CSS -->
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css'>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+        <title> Serpisan | Home </title>
+
+        <style>
+
+        
+        </style>
+    </head>
+    <body style="font-family: Nunito; background-color: #f9f9f9;"">
+
+
+
+    <div class="container" style="margin-top: 50px;">
+        <div class="row">
+            <div class="col">
+            </div>
+            <div class="col">
+                <h1 style="text-align: center; font-weight:bold;">About Us</h1>
+            </div>
+            <div class="col">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-8">
+                <h4 style="text-align: center;">Serpisan adalah situs penyedia layanan jasa service elektronik yang mudahkan bagi para customer
+                mendapatkan informasi yang Mudah, Simple dan Uptudate.<br> Ini adalah hasil tugas akhir Mata Kuliah Pemograman Web Semester 4 Program Studi Teknik Informatika
+                Fakultas Teknologi Informasi dan Komunikasi Universitas Semarang</h4>
+            </div>
+            <div class="col"></div>
+        </div><br><br>
+        <div class="row mt-5">
+            <div class="col"></div>
+            <div class="col-8">
+                <h4 style="text-align: center; color:#02668e;">Dibuat dengan sepenuh <i class='bx bxs-heart' style='color:#ff0000' ></i>  oleh </h4>
+            </div>
+            <div class="col"></div>
+        </div>
+
+        <!--gallery-->
+        <hr class="mt-2 mb-5">
+
+        <div class="row text-center text-lg-start">
+
+            <div class="col-lg-3 col-md-4 col-6">
+                <img class="img-fluid img-thumbnail" src="images/monyet3.jpg" alt="">
+                <p style="text-decoration: none;"> Imad Muhammad Ridlo <br> G.211.20.0063 </p>
+            </div>
+            <div class="col-lg-3 col-md-4 col-6">
+                <img class="img-fluid img-thumbnail" src="images/monyet2.jpg" alt="">
+                <p style="text-decoration: none;"> Muhammad Ghozali Khusyaini <br> G.211.20.0096</p>
+            </div>
+            <div class="col-lg-3 col-md-4 col-6">
+                <img class="img-fluid img-thumbnail" src="images/monyet1.jpg" alt="">
+                <p style="text-decoration: none;"> Abdur Rofiq <br> G.211.20.0115 </p>
+            </div>
+            <div class="col-lg-3 col-md-4 col-6">
+                <img class="img-fluid img-thumbnail" src="images/monyet5.jpg" alt="">
+                <p style="text-decoration: none;"> Yovi Adhi Ramadani<br>  G.211.20.0116 </p>
+            </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+    <br><br><br><br><br><br><br><br><br>
+
+
+
+<?php 
+    include 'bawah.php';
+?>
+
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+-->
+</body>
+</html>
