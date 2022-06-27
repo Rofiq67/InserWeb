7862614d8559b85eaca5965ef8685c3507ebb37d
Fiq <75403740+Rofiq67@users.noreply.github.com>
2022-06-27 11:36:20 +0700

    Update navcustomer.php

--- php/navcustomer.php
+++ php/navcustomer.php
@@ -4,18 +4,21 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title> Serpisan | Admin </title>
+
+        <script type="text/javascript" src="Scripts/jquery-2.1.1.min.js"></script>
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <!-- Google Font -->
         <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
         <!-- Box Icon -->
         <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-        <title> Serpisan | Customer </title>
-        <style>
         
-        </style>
     </head>
     <body style="font-family: Nunito; background-color: #f9f9f9;">
+
+
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white" >
         <div class="container-fluid" style="margin-left: 77px;">
@@ -29,37 +32,42 @@
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item" style="margin-right: 32px;">
-                <a class="nav-link" href="CekService.php">Cek Service</a>
+                <a class="nav-link" href="tabeladmin.php">Cek Service</a>
                 </li>
                 <li class="nav-item" style="margin-right: 32px;">
-                <a class="nav-link" href="#">Layanan</a>
-                </li>
-                <li class="nav-item" style="margin-right: 32px;">
-                <a class="nav-link" href="#">About me</a>
+                <a class="nav-link" href="#aboutme">About me</a>
                 </li>
                 </li>
                 <li class="nav-item" style="margin-right: -4px;">
                     <a class="nav-link" href="#"><i class='bx bxs-user-circle' style="font-size: 15pt;"></i></a>
                 </li>
-                <li class="nav-item" style="margin-right:0px;">
-                <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
                 <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Customer
-                </a>
-                <ul class="dropdown-menu dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink" >
-                    <li><a class="dropdown-item" href="#"style="font-color:black;"><b>Profil</b></a></li>
-                    <li><a class="dropdown-item" href="Loginpage.php"><i class='bx bx-log-out'></i> <b>Log Out</b></a></li>
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Admin
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="logout.php">Sign Out</a></li>
+                    </ul>
                 </li>
             </ul>
             </div>
         </div>
     </nav>
     
+
     
+
+
+
+
+
+
+
     <!-- Optional JavaScript; choose one of the two! -->
+
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
